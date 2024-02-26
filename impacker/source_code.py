93fbbafbb373b2c0774e6a24c0907bd2fd4c7934
Jimin Park <pjm950@gmail.com>
2024-02-27 00:08:28 +0900

    Make SourceCode.from_path accept strings.

--- impacker/source_code.py
+++ impacker/source_code.py
@@ -57,7 +57,9 @@ class SourceCode():
         self.imports.add(ast)
     
     @staticmethod
-    def from_path(src_path: Path):
+    def from_path(src_path: Path|str):
+        if not isinstance(src_path, Path):
+            src_path = Path(src_path)
         spec = spec_from_file_location(src_path.stem, src_path)
         return SourceCode(spec) if spec else None
 
